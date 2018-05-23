250821f91e65de20dc64b3cf16b1d542c06f9b1b
Gonzalo B <gonzalobusnadiego@gmail.com>
2018-05-23 13:45:09 -0300

    ssl off
    Ssl off

--- src/MeliManager.php
+++ src/MeliManager.php
@@ -28,7 +28,7 @@ class MeliManager
      */
     public static $CURL_OPTS = array(
         CURLOPT_USERAGENT => "MELI-PHP-SDK-1.0.0",
-        CURLOPT_SSL_VERIFYPEER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_CONNECTTIMEOUT => 10,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_TIMEOUT => 60
